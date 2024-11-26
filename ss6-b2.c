d542b3c779e82443e8efded39616631db5784eda
Đăng Việt <trandangviet00@gmail.com>
2024-11-27 01:33:20 +0700

    Add files via upload

---
+++ ss6-b2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n[5];       
+    int so_chan = 0;    
+    int so_le = 0;     
+
+    printf("Nhap vao 5 so nguyen bat ky:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap so thu %d: ", i + 1);
+        scanf("%d", &n[i]);
+
+        if (n[i] % 2 == 0) {
+            so_chan++;   
+        } else {
+            so_le++;   
+        }
+    }
+
+    
+    printf("So luong so chan: %d\n", so_chan);
+    printf("So luong so le: %d\n", so_le);
+
+    return 0;
+}
