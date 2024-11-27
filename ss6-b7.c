a6020aa1ebeba00de35c45b1808850f74cac4a2c
Đăng Việt <trandangviet00@gmail.com>
2024-11-27 10:09:29 +0700

    Add files via upload

---
+++ ss6-b7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &n);
+    printf("Các uoc cua so %d la: ", n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
